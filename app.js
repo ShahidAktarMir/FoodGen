a57899a912a5399bd2c9cc6f2b649f91cc8c1644
Shahid Aktar Mir <shahidaktarm@gmail.com>
2023-02-18 19:04:28 +0530

    Create Functional Components

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 const fullName = <h1>Shahid Aktar Mir</h1>
+const ShortName = ()=>{
+    return(
+        <>
+        {fullName}
+        <div>Shahid</div>
+        </>
+        
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(fullName);
+root.render(<ShortName/>);
