1994671c63792ba5b15ffab77ebc721bc5249c60
Shahid Aktar Mir <shahidaktarm@gmail.com>
2023-02-18 18:34:36 +0530

    Added parcel -DEV, react, react-DOM Using NPM

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const firstName = React.createElement('h1',null,"Shahid");
-const midName = React.createElement('h1',null,"Aktar");
-const lastName = React.createElement('h1',null,"Mir");
-const fullName = React.createElement('div',null,[firstName,midName,lastName]); 
-
+import React from "react";
+import ReactDOM  from "react-dom/client";
+const fullName = <h1>Shahid Aktar Mir</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(fullName);
