ba63a45493cc4d2073f015b426ff155573f94fda
Shahid Aktar Mir <shahidaktarm@gmail.com>
2023-02-19 20:28:03 +0530

    Fetch Data and Render

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,24 +2,26 @@ import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 const Body =()=>{
-    const [allRes, setAllres] = useState();
+    const [allRes, setAllres] = useState([]);
     const [filterRes, setFilterres] = useState();
     useEffect(()=>{
         getRes();
     },[])
 
     async function getRes(){
-        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5690538&lng=88.40904429999999&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING')
-        console.log(data);
-        const json = await data.json;
-        console.log(json);
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4935958&lng=88.1949782&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json();
+        setAllres(json?.data?.cards[1]?.data?.data?.cards);
     }
-
     return(
-        <>
-        <h1>Shahid</h1>
-        <RestaurantCard/>
-        </>
+        <div>{
+            allRes.map((res)=>{
+                return(
+                    <RestaurantCard {...res?.data} key = {res?.data?.id}/>
+                )
+            })
+        }</div>
+        
     )
 }
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,7 +1,8 @@
-const RestaurantCard = () =>{
+const RestaurantCard = ({name,cloudinaryImageId,costForTwoString,cuisines,locality}) =>{
     return(
         <div className="cards">
-
+        <h1>{name}</h1>
+        <h1>{locality}</h1>
         </div>
     );
 }
