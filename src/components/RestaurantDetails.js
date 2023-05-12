4b23efb3ba5d51f342c7d1d4cff2530933d28249
Shahid Aktar Mir <shahidaktarm@gmail.com>
2023-05-12 20:40:57 +0530

    Fixed CSS

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -47,14 +47,13 @@ const ResDet = () => {
     setKeyword(keyword);
     setFilterMenu(filtered);
   };
-
   return !mainRes ? (
     <h1>Wait for While</h1>
   ) : (
-    <>
+    <div>
       <SearchBar keyword={keyword} onChange={updateKeyword} />
-      <div className=" m-28 flex space-x-40">
-        <div className="bg-slate-100 p-5 rounded-2xl h-full">
+      <div className="m-10 flex flex-wrap space-x-10">
+        <div className="bg-gray-100 p-5 border-2 border-x-red-500 border-y-green-100 hover:border-y-green-500 hover:border-x-red-100 rounded-2xl h-3/4 w-96 transition-all duration-500 ease-in-out">
           <img
             src={IMGID + mainRes.cloudinaryImageId}
             className="rounded-t-lg mb-10"
@@ -64,19 +63,15 @@ const ResDet = () => {
           <h6>{mainRes.city}</h6>
           <h4>Rating: {mainRes.avgRating}</h4>
           <h4>Locality: {mainRes.locality}</h4>
-          <p className="text-green-500 mt-10 bg-gray-200 rounded-full p-5 text-center font-semibold">
+          <p className="text-green-500 cursor-pointer hover:bg-green-500 transition-all duration-500 ease-in-out hover:text-gray-100 mt-10 bg-gray-200 rounded-full p-5 text-center font-semibold">
             ₹ {mainRes.costForTwo / 200}
           </p>
         </div>
-        <div>
-          <p>
-            <b>
-              <i>Menus -</i>
-            </b>
-          </p>
+        <div className=" p-2 rounded-lg  w-3/4">
+          <p className="text-center">Menus</p>
           {filterMenu.map((menu) => {
             return (
-              <div className="grid grid-cols-6 gap-4">
+              <div className="grid grid-cols-6 gap-4 border-2 border-x-orange-200 bg-slate-50 p-5 m-5 rounded-lg">
                 <img
                   src="https://cdn.shopify.com/s/files/1/0050/5508/6656/products/2000px-Indian-vegetarian-mark.svg_aa6ffbb3-9caf-482d-a679-147a3af521b0.png?v=1663136192&width=1946"
                   className="h-5 mt-14"
@@ -91,7 +86,7 @@ const ResDet = () => {
                 />
                 {/* {console.log(menu?.card?.info?.imageId)} */}
                 <p className="mt-10 ml-10">₹{menu?.card?.info?.price / 200}</p>
-                <button className="bg-green-300 m-3 ml-10 p-1 h-8 mt-10 ">
+                <button className="bg-green-300 m-3 ml-10 p-1 h-8 mt-10 hover:bg-green-400 transition-all duration-100">
                   Add to cart
                 </button>
               </div>
@@ -99,7 +94,7 @@ const ResDet = () => {
           })}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default ResDet;
